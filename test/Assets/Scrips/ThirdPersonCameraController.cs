78adc92662cd2ad3e9dc3b788f0439b0b258c3e1
maoche <maoche001@gmail.com>
2020-02-12 15:00:21 +0800

    0212

---
+++ test/Assets/Scrips/ThirdPersonCameraController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    public float RotationSpeed;
+    public Transform player, target;
+    float mouseX, mouseY;
+
+    //穿牆內容
+    public Transform Obstruction;
+    float zoomspeed = 2.0f;
+
+    private void Start()
+    {
+        Obstruction = target;
+        Cursor.visible = false;//鼠標消失
+        Cursor.lockState = CursorLockMode.Locked;//鼠標限制在視窗
+    }
+    private void LateUpdate()
+    {
+        CamControl();
+        ViewObstructed();
+    }
+    void CamControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * RotationSpeed;//定義滑鼠X數值
+        mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;//定義滑鼠Y數值
+        mouseY = Mathf.Clamp(mouseY, -35, 60);//限制角度
+
+        transform.LookAt(target);
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        }
+       else
+        {
+            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+    }
+
+    //穿牆
+    void ViewObstructed()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, target.position - transform.position, out hit, 4.5f))
+        {
+            if(hit.collider.gameObject.tag != "Player")
+            {
+                Obstruction = hit.transform;
+                Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+
+                //if(Vector3.Distance(Obstruction.position,transform.position) >= 3f && Vector3.Distance(transform.position,target.position) >= 1.5f)
+                //{
+                  //  transform.Translate(Vector3.forward * zoomspeed * Time.deltaTime);
+                //}
+            }
+            else
+            {
+                Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+               // if(Vector3.Distance(transform.position,target.position) < 4.5f)
+                //{
+                  //  transform.Translate(Vector3.back * zoomspeed * Time.deltaTime);
+                //}
+            }
+        }
+    }
+}
---
+++ test/Assets/Scrips/ThirdPersonCharactorController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharactorController : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rig;
+    public float jumpspeed;
+    public bool isGround;
+    float mouseX, mouseY;
+    public float RotationSpeed;
+    public Transform player;
+
+    void Update()
+    {
+        PlayerMovement();
+    }
+    void PlayerMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 PlayerMovement = new Vector3(h, 0f, v) * speed * Time.deltaTime;
+        transform.Translate(PlayerMovement, Space.Self);
+        mouseX += Input.GetAxis("Mouse X") * RotationSpeed*Time.deltaTime;//定義滑鼠X數值
+        // mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;//定義滑鼠Y數值
+       // mouseY = Mathf.Clamp(mouseY, -35, 60);//限制角度
+        //target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        player.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+
+}
