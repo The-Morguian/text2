6250be2f8aca37f70408c0a70f5835a9332ec7b0
maoche <maoche001@gmail.com>
2020-02-20 15:02:20 +0800

    0220

---
+++ test/Assets/Horizontalscroll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Horizontalscroll : MonoBehaviour
+{
+    public Rigidbody rig;
+    public Transform tran;
+    public float speed;
+
+    void Start()
+    {
+        PlayerMove();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void PlayerMove()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        rig.AddForce(tran.forward * speed * v * Time.deltaTime);
+    }
+}
